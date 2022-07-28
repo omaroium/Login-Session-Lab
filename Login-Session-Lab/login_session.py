f4fdbc25a3c9a67901f091187433aac6c4fd65f8
omaroium <omaralimycan@gmail.com>
2022-07-28 16:21:02 +0300

    rany useless

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,10 +4,29 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/',methods=['POST','GET'] ) # What methods are needed?
 def home():
-	
+	if request.method=="POST":
+		i=0;
+		names=[]
+		quotes=[]
+		ages=[]
+		quotes.append(request.form['quote'])
+		ages.append(request.form['age'])
+		names.append(request.form['name'])
+		try:
+			login_session['name'] = names[0]
+			login_session['age'] = ages[0]
+			login_session['quote'] = quotes[0]
+		
+			if names[0]== "" or ages[0]==""or quotes[0]=="":
+				return redirect(url_for('error'))
+			else:
+				return redirect(url_for('thanks'))
+		except:
+			return redirect(url_for('error'))
 	return render_template('home.html')
+
 
 
 @app.route('/error')
@@ -18,8 +37,7 @@ def error():
 
 @app.route('/display')
 def display():
-
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html',name=login_session['name'],age=login_session['age'],quote=login_session['quote']  , len=len(login_session['name'])) # What variables are needed?
 
 
 @app.route('/thanks')
